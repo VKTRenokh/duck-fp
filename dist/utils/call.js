d45b2ee306ffd31a990308ed8c85f26da1e9860d
VKTRenokh <viktorenokh@gmail.com>
2024-02-14 13:37:40 +0200

    refactor(dist): add dist to gitignore

--- dist/utils/call.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.call = void 0;
-const call = (fn) => fn();
-exports.call = call;
--- dist/utils/is-callable.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isCallable = void 0;
-const isCallable = (fn) => typeof fn === "function";
-exports.isCallable = isCallable;
