dabad19bf7abd3734d3e5c075fbc0452f59a1fe4
VKTRenokh <viktorenokh@gmail.com>
2024-02-15 08:13:26 +0200

    refactor(or): remove useless or-reverse function

---
+++ .changeset/giant-icons-move.md
@@ -0,0 +1,5 @@
+---
+"@victorenokh/maybe.ts": patch
+---
+
+Removed or-reverse function
--- src/utils/or.ts
+++ src/utils/or.ts
@@ -2,7 +2,6 @@ import { Maybe, maybe } from 'src/maybe'
 
 /**
  * Combines multiple Maybe monads into one, returning the first non-empty Maybe monad.
- * This function prioritizes the Maybe monads in the order they are provided.
  * @template MT - The types of the Maybe monads to combine.
  * @param {...MT[]} maybies - The Maybe monads to combine.
  * @returns {MT[number]} The first non-empty Maybe monad, or `null` if all input Maybe monads are empty.
@@ -10,7 +9,8 @@ import { Maybe, maybe } from 'src/maybe'
 export const or = <MT extends Maybe<any>[]>(
   ...maybies: MT
 ): MT[number] => {
-  return maybies.reduce((acc, curr) => {
-    return acc.flatGetOrElse(curr)
-  }, maybe<MT[number]>(null))
+  return maybies.reduce(
+    (acc, curr) => acc.flatGetOrElse(curr),
+    maybe<MT[number]>(null),
+  )
 }
