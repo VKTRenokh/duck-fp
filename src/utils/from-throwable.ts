54fb485919f9bb59f5d8a13ad69cfe53b2620186
VKTRenokh <viktorenokh@gmail.com>
2024-02-15 17:28:55 +0200

    feat: implement from-throwable

---
+++ src/utils/from-throwable.ts
@@ -0,0 +1,20 @@
+import { maybe, none } from 'src/maybe'
+
+/**
+ * Wraps a potentially throwing function in a Maybe monad.
+ * @template T - The types of the arguments accepted by the function.
+ * @template R - The return type of the function.
+ * @param {(...a: T) => R} f - The function to wrap.
+ * @returns {(...a: T) => Maybe<R>} A new function that returns a Maybe monad containing the result of the wrapped function, or `none` if an error occurs.
+ */
+export const fromThrowable =
+  <T extends ReadonlyArray<unknown>, R>(
+    f: (...a: T) => R,
+  ) =>
+  (...a: T) => {
+    try {
+      return maybe(f(...a))
+    } catch (e) {
+      return none<R>()
+    }
+  }
