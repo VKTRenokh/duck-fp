b0ba718e0bf1c30668699f9fa76f0f678079d307
VKTRenokh <viktorenokh@gmail.com>
2024-02-17 21:14:01 +0200

    feat: implement from-throwable for either monad

---
+++ src/either/utils/from-throwable.ts
@@ -0,0 +1,13 @@
+import { Either, left, right } from '../either'
+
+export const fromThrowable =
+  <T extends ReadonlyArray<unknown>, R>(
+    f: (...a: T) => R,
+  ) =>
+  (...a: T): Either<unknown, R> => {
+    try {
+      return right(f(...a))
+    } catch (e) {
+      return left(e)
+    }
+  }
