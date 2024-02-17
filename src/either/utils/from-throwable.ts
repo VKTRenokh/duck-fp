1df5db9b95e1a6f4e7b2a3643e9611792e51aa94
VKTRenokh <viktorenokh@gmail.com>
2024-02-17 22:07:13 +0200

    feat: add jsdoc for from-throwable either

--- src/either/utils/from-throwable.ts
+++ src/either/utils/from-throwable.ts
@@ -1,5 +1,12 @@
 import { Either, left, right } from '../either'
 
+/**
+ * Constructs a function that converts a throwable function into an `Either`.
+ * @param {(...a: T) => R} f The function to be wrapped.
+ * @returns {(...a: T) => Either<unknown, R>} A function that takes the same parameters as `f` and returns an `Either`.
+ * @template T The types of the parameters of the function `f`.
+ * @template R The return type of the function `f`.
+ */
 export const fromThrowableE =
   <T extends ReadonlyArray<unknown>, R>(
     f: (...a: T) => R,
