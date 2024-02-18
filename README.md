f5e5862a2a992134249f312eb5be17831798c930
VKTRenokh <viktorenokh@gmail.com>
2024-02-18 20:57:43 +0200

    feat: add jsdoc for Right, Left interface methods

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ To install the stable version:
 `npm i @victorenokh/maybe.ts`
 
 # Examples
-- [maybe](./examples/maybe.md)
-- [either](./examples/either.md)
+- [Maybe](./examples/maybe.md)
+- [Either](./examples/either.md)
 
 # License
 The MIT License (MIT)
--- src/either/either.ts
+++ src/either/either.ts
@@ -16,23 +16,111 @@ export type FilterOrElseFunction<Left, Right> = (
   fr: () => Left,
 ) => Either<Left, Right>
 
+/**
+ * Represents the left side of the Either monad.
+ * @template T - The type of the left value.
+ * @template R - The type of the right value.
+ */
 export interface Left<T, R> {
   readonly left: T
+
+  /**
+   * Checks if this side of Either is a Right.
+   * @returns {boolean} - Returns true if this side is right.
+   */
   isRight: () => false
+
+  /**
+   * Checks if this side of Either is a Left.
+   * @returns {boolean} - Returns true if this side is left.
+   */
   isLeft: () => true
+
+  /**
+   * Folds over either side of the Either monad.
+   * @template R - The type to fold into.
+   * @param {Function} lfn - The function to map over the left side.
+   * @param {Function} rfn - The function to map over the right side (unused in Left).
+   * @returns {R} - The result of applying `leftFn` to the left value.
+   */
   fold: FoldFunction<T, R>
+
+  /**
+   * Maps a function over the left value.
+   * @template U - The type of the result of the mapping function.
+   * @param {Function} fn - The function to map over the left value.
+   * @returns {Left<U, R>} - A new Left containing the result of applying `fn` to the left value.
+   */
   map: MapFunction<T, R>
+
+  /**
+   * Maps a function over the left value and flattens the result.
+   * @template U - The type of the result of the mapping function.
+   * @param {Function} fn - The function to map over the left value.
+   * @returns {Left<U, R>} - A new Left containing the flattened result of applying `fn` to the left value.
+   */
   flatMap: FlatMapFunction<T, R>
+
+  /**
+   * Filters the right value with the provided predicate.
+   * @param {Function} predicate - The predicate function to test the left value.
+   * @param {any} orElse - The value to return if the predicate does not hold.
+   * @returns {Left<T, R>} - A new Left if the predicate holds, otherwise returns itself.
+   */
   filterOrElse: FilterOrElseFunction<T, R>
 }
 
+/**
+ * Represents the right side of the Either monad.
+ * @template T - The type of the right value.
+ * @template R - The type of the left value.
+ */
 export interface Right<T, R> {
   readonly right: T
+
+  /**
+   * Checks if this side of Either is a Right.
+   * @returns {boolean} - Returns true if this side is right.
+   */
   isRight: () => true
+
+  /**
+   * Checks if this side of Either is a Left.
+   * @returns {boolean} - Returns true if this side is left.
+   */
   isLeft: () => false
+
+  /**
+   * Folds over either side of the Either monad.
+   * @template U - The type to fold into.
+   * @param {Function} _leftFn - The function to map over the left side (unused in Right).
+   * @param {Function} rightFn - The function to map over the right side.
+   * @returns {U} - The result of applying `rightFn` to the right value.
+   */
   fold: FoldFunction<R, T>
+
+  /**
+   * Maps a function over the right value.
+   * @template U - The type of the result of the mapping function.
+   * @param {Function} fn - The function to map over the right value.
+   * @returns {Right<T, U>} - A new Right containing the result of applying `fn` to the right value.
+   */
   map: MapFunction<R, T>
+
+  /**
+   * Maps a function over the right value and flattens the result.
+   * @template U - The type of the result of the mapping function.
+   * @param {Function} fn - The function to map over the right value.
+   * @returns {Right<T, U>} - A new Right containing the flattened result of applying `fn` to the right value.
+   */
   flatMap: FlatMapFunction<R, T>
+
+  /**
+   * Filters the right value with the provided predicate.
+   * @param {Function} predicate - The predicate function to test the right value.
+   * @param {any} orElse - The value to return if the predicate does not hold.
+   * @returns {Right<T, R>} - A new Right if the predicate holds, otherwise returns itself.
+   */
   filterOrElse: FilterOrElseFunction<R, T>
 }
 
