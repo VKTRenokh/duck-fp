5c9bc393eb1de1e5305a64843aba6706f4780099
VKTRenokh <viktorenokh@gmail.com>
2024-03-08 00:02:36 +0200

    feat: implement reader monad

---
+++ src/reader/reader.ts
@@ -0,0 +1,14 @@
+export interface Reader<T, R> {
+  run: (env: T) => R
+  map: <B>(fn: (v: R) => B) => Reader<T, B>
+  flatMap: <B>(fn: (v: R) => Reader<T, B>) => Reader<T, B>
+}
+
+export const of = <T, R>(
+  run: (env: T) => R,
+): Reader<T, R> => ({
+  run,
+  map: <B>(fn: (v: R) => B) => of((env: T) => fn(run(env))),
+  flatMap: <B>(fn: (v: R) => Reader<T, B>) =>
+    of((env: T) => fn(run(env)).run(env)),
+})
