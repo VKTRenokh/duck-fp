8b92082f86908db1c910277c283bb8a2baa24cfe
VKTRenokh <viktorenokh@gmail.com>
2024-03-08 23:03:47 +0200

    feat: add Reader#flatMap test

--- tests/reader.test.ts
+++ tests/reader.test.ts
@@ -1,5 +1,9 @@
 import { R } from '../src'
 
+interface Env {
+  s: number
+}
+
 describe('reader.ts', () => {
   it('run', () => {
     const run = jest.fn(
@@ -16,15 +20,24 @@ describe('reader.ts', () => {
   })
 
   it('map', () => {
-    interface Env {
-      s: number
-    }
-
     const run = jest.fn((env: Env) => env.s * 2)
 
     const reader = R.of(run).map((number) => number / 0.2)
 
     expect(reader.run({ s: 100 })).toBe(1000)
     expect(run).toHaveBeenCalled()
+  })
+
+  it('flatMap', () => {
+    const a = R.of<Env, number>((env) => env.s * 10)
+    const b = R.of<Env, number>((env) => env.s * 2)
+
+    const merged = a.flatMap((firstValue) =>
+      b.map(
+        (secondValue) => [firstValue, secondValue] as const,
+      ),
+    )
+
+    expect(merged.run({ s: 10 })).toStrictEqual([100, 20])
   })
 })
