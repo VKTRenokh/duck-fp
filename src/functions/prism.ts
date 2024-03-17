777df2b79150e1dfdc7f16af857e12e4038e8ad8
VKTRenokh <viktorenokh@gmail.com>
2024-03-17 21:08:50 +0200

    feat: implement composePrism function

--- src/functions/prism.ts
+++ src/functions/prism.ts
@@ -3,7 +3,7 @@ import { of } from 'src/maybe'
 
 export interface Prism<T, R> {
   view: (v: T) => Maybe<R>
-  set: (v: T, s: R) => R
+  set: (v: R, s: T) => T
 }
 
 export const prism = <T, R>(
@@ -12,4 +12,14 @@ export const prism = <T, R>(
 ): Prism<T, R> => ({
   view: (v) => of(view(v)),
   set,
+})
+
+export const composePrism = <A, B, C>(
+  ab: Prism<A, B>,
+  bc: Prism<B, C>,
+): Prism<A, C> => ({
+  view: (a) => ab.view(a).flatMap((view) => bc.view(view)),
+  set: (c, a) =>
+    ab.view(a).map((view) => ab.set(bc.set(c, view), a))
+      .value!,
 })
--- src/maybe/utils/from-throwable.ts
+++ src/maybe/utils/from-throwable.ts
@@ -5,7 +5,7 @@ import { of, none } from '../maybe'
  * @template T - The types of the arguments accepted by the function.
  * @template R - The return type of the function.
  * @param {(...a: T) => R} f - The function to wrap.
- * @returns {(...a: T) => Maybe<R>} A new function that returns a Maybe monad containing the result of the wrapped function, or `none` if an error occurs.
+ * @returns A new function that returns a Maybe monad containing the result of the wrapped function, or `none` if an error occurs.
  */
 export const fromThrowable =
   <T extends ReadonlyArray<unknown>, R>(
