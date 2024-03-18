140b37ab41e945b5c1e3064286e495f34a97c1ce
VKTRenokh <viktorenokh@gmail.com>
2024-03-18 23:21:12 +0200

    refactor: move compose function into Lens

--- src/functions/lens.ts
+++ src/functions/lens.ts
@@ -1,31 +1,29 @@
 export interface Lens<T, R> {
   view: (v: T) => R
   set: (v: R, s: T) => T
+  compose: <C>(bc: Lens<R, C>) => Lens<T, C>
 }
 
-export const lens = <T, R>(
+export const from = <T, R>(
   view: Lens<T, R>['view'],
   set: Lens<T, R>['set'],
 ): Lens<T, R> => ({
   view,
   set,
+  compose: <C>(bc: Lens<R, C>): Lens<T, C> =>
+    from(
+      (a) => bc.view(view(a)),
+      (c, a) => set(bc.set(c, view(a)), a),
+    ),
 })
 
-export const lensKey = <
+export const fromProp = <
   K extends symbol | string | number,
   T extends Record<K, unknown>,
 >(
   key: K,
 ): Lens<T, T[K]> =>
-  lens<T, T[K]>(
+  from<T, T[K]>(
     (v) => v[key],
     (v, s) => ({ ...s, [key]: v }),
   )
-
-export const composeLens = <A, B, C>(
-  ab: Lens<A, B>,
-  bc: Lens<B, C>,
-): Lens<A, C> => ({
-  view: (a) => bc.view(ab.view(a)),
-  set: (c, a) => ab.set(bc.set(c, ab.view(a)), a),
-})
