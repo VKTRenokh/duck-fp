215ff609258ba23ad7a00cead4699e4ef3d6b266
VKTRenokh <viktorenokh@gmail.com>
2024-03-25 16:38:32 +0200

    feat: implement TaskMaybe#map & TaskMaybe#flatMap methods

--- src/task-maybe/task-maybe.ts
+++ src/task-maybe/task-maybe.ts
@@ -1,13 +1,25 @@
 import { M } from '-->'
 import { Lazy } from '->t/lazy'
-import { Maybe } from '->/maybe'
+import { none } from '->/maybe'
 
 interface TaskMaybe<T> {
-  run: () => Promise<Maybe<T>>
+  run: () => Promise<M.Maybe<T>>
+  map: <R>(f: (v: T) => R) => TaskMaybe<R>
+  flatMap: <R>(f: (v: T) => TaskMaybe<R>) => TaskMaybe<R>
 }
 
 export const of = <T>(
-  run: Lazy<Promise<T | null>>,
+  run: Lazy<Promise<M.Maybe<T>>>,
 ): TaskMaybe<T> => ({
-  run: () => run().then(M.of),
+  run: run,
+  map: <R>(f: (v: T) => R): TaskMaybe<R> =>
+    of(() => run().then((value) => value.map(f))),
+  flatMap: <R>(f: (v: T) => TaskMaybe<R>): TaskMaybe<R> =>
+    of(() =>
+      run().then((maybe) =>
+        maybe.value !== null
+          ? f(maybe.value).run()
+          : none<R>(),
+      ),
+    ),
 })
