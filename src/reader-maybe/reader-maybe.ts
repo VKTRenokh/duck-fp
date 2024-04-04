a498c184f816855f2fdc7dcd6618e3bbb289af5a
VKTRenokh <viktorenokh@gmail.com>
2024-04-04 14:00:47 +0300

    feat: implement ReaderMaybe#orElse

--- src/reader-maybe/reader-maybe.ts
+++ src/reader-maybe/reader-maybe.ts
@@ -3,6 +3,7 @@ import { Maybe, none } from '->/maybe'
 interface ReaderMaybe<E, T> {
   run: (e: E) => Maybe<T>
   map: <R>(f: (v: T) => R) => ReaderMaybe<E, R>
+  orElse: (or: ReaderMaybe<E, T>) => ReaderMaybe<E, T>
   flatMap: <R>(
     f: (v: T) => ReaderMaybe<E, R>,
   ) => ReaderMaybe<E, R>
@@ -14,6 +15,8 @@ export const of = <E, T>(
   run,
   map: <R>(f: (v: T) => R): ReaderMaybe<E, R> =>
     of((e) => run(e).map(f)),
+  orElse: (or: ReaderMaybe<E, T>): ReaderMaybe<E, T> =>
+    of((e) => run(e).flatGetOrElse(or.run(e))),
   flatMap: <R>(
     f: (v: T) => ReaderMaybe<E, R>,
   ): ReaderMaybe<E, R> =>
