799ef9043c314bc0d03247b307f656891c609ccc
VKTRenokh <viktorenokh@gmail.com>
2024-04-04 17:33:30 +0300

    feat: add ReaderMaybe#runOrElse test

--- tests/reader-maybe.test.ts
+++ tests/reader-maybe.test.ts
@@ -1,6 +1,7 @@
 import * as M from '../src/maybe'
 import { ReaderMaybe, of } from '../src/reader-maybe'
 
+// {{{ helpers
 interface Env {
   something?: string
 }
@@ -10,6 +11,7 @@ const eNullable: Env = {}
 const e: Env = {
   something: 'test',
 }
+// }}}
 
 describe('reader-maybe.ts', () => {
   // {{{ map
@@ -27,6 +29,44 @@ describe('reader-maybe.ts', () => {
 
     expect(mapped.run(e).value).toBe('testtest')
     expect(mapFn).toHaveBeenCalledWith('test')
+  })
+  // }}}
+  // {{{ flatMap
+  it('flatMap', () => {
+    const reader = of<Env, string>((e) =>
+      M.fromUndefined(e.something),
+    )
+    const otherReader = of<Env, string>((e) =>
+      M.fromUndefined(e.something).map((e: string) =>
+        e.repeat(2),
+      ),
+    )
+
+    const flatMapFn = jest.fn(() => otherReader)
+
+    const runned = reader.flatMap(flatMapFn).run(e)
+
+    expect(runned.value).toBe('testtest')
+    expect(runned.isNothing()).toBeFalsy()
+  })
+  // }}}
+  // {{{ runOrElse
+  it('runOrElse', () => {
+    const reader = of<Env, string>((e) =>
+      M.fromUndefined(e.something),
+    )
+
+    const runnedNonNullable = reader.runOrElse(
+      e,
+      'it is not possible',
+    )
+    const runnnedNullable = reader.runOrElse(
+      eNullable,
+      'should be this',
+    )
+
+    expect(runnedNonNullable).toBe('test')
+    expect(runnnedNullable).toBe('should be this')
   })
   // }}}
 })
